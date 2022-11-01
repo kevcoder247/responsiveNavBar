dd965e6395b6b5d58a3f655f869b65af8df792e3
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2022-11-01 13:19:50 -0400

    markup and css set up files added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log('hello')
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
